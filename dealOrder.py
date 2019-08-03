e71831fae48dfba08d72dd098581e4caf5a10962
daiqile <daiqile@xwzsz.com>
2019-08-03 17:39:52 +0800

    修改撤单逻辑，增加异常处理

--- dealOrder.py
+++ dealOrder.py
@@ -1,10 +1,11 @@
 # _*_ coding:utf-8 _*_
-from order import order,getOrder,getOpenOrders,getBalance,cancelOrder
+from order import order, getOrder, getOpenOrders, getBalance, cancelOrder
 import random
 import time
 
 price = 1023
 is_stop = True
+
 
 def buy_order():
     while is_stop:
@@ -15,6 +16,8 @@ def buy_order():
         time.sleep(0.2)
         result = order(num, "eth_usdt$@#@&&&", buy_price, "buy")
         print(result)
+
+
 def sell_order():
     while is_stop:
         num = random.uniform(2, 100)
@@ -27,20 +30,24 @@ def sell_order():
 
 
 def getOrders(type):
-    for i in range(1,8):
-        results = getOpenOrders("eth_usdt",i,type)
-        for result in results:
-            if result == "":
-                return
-            id  = result['id']
-            time.sleep(0.5)
-            ss = cancelOrder("eth_usdt",id)
-            print(ss)
-
+    for i in range(1, 8):
+        i = str(i)
+        results = getOpenOrders("eth_usdt", i, type)
+        try:
+            if results:
+                for result in results:
+                    if result:
+                        id = result['id']
+                        time.sleep(0.5)
+                        ss = cancelOrder("eth_usdt", id)
+                        print(ss)
+        except Exception as e:
+            print("撤掉失败", e)
 
 
 if __name__ == "__main__":
     getOrders("buy")
+    getOrders("sell")
 
 
 
