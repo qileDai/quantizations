48a0a84ec9d536b8a981f6528be28c8980a2f8ed
daiqile <daiqile@xwzsz.com>
2019-08-05 11:38:19 +0800

    规划目录结构

---
+++ exchangeConnection/exx/exxService.py
@@ -0,0 +1,152 @@
+# _*_ coding:utf-8 _*_
+import requests
+from requests import ConnectionError,ReadTimeout
+from exchangeConnection.exx.util import sha512_signature
+import time
+
+from utils import restful
+from utils import Logger
+import accountConfig
+
+log = Logger.MylogHandler("exxdeal")
+
+
+
+"""
+Exx交易平台交易类
+"""
+baseUrl = accountConfig.EXX["SERVICE_API"]
+secretKey = accountConfig.EXX["SECRET_KEY"]
+accesskey = accountConfig.EXX["ACCESS_KEY"]
+
+
+"""
+委托下单
+:param: amount 交易数量
+:param: currency:eth_usdt 交易的对
+:param: price:1024 价格
+:param: type:buy/sell 交易类型
+:return: int 交易id
+"""
+def exx_order(amount, currency, price, type):
+    current_time = str(int(time.time() * 1000))
+    if isinstance(amount,int):
+        amount = str(amount)
+    print(current_time)
+    try:
+        params = "&amount=" + amount + "&currency=" + currency + "&price=" + price + "&type=" + type
+        url = baseUrl + "order" + "?" + accesskey + params + \
+              "&nonce=" + current_time + "&signature=" + sha512_signature(params)
+        response = requests.get(url)
+        result = response.json()
+        try:
+            response = requests.get(url)
+            result = response.json()
+        except(ConnectionError, ReadTimeout) as e:
+            log.error("请求下单链接失败",e)
+        # 签名验证不通过，开始重新3次连接，3次连接不成功退出连接
+        if result['code'] == 103:
+            for i in range(1, 4):
+                response = requests.get(url)
+                result = response.json()
+                log.info(result)
+                if result['code'] == 103:
+                    return
+                elif result['code'] == 100:
+                    return result
+    except Exception as e:
+        log.error("委托下单失败",e)
+    return result
+
+
+
+"""
+取消委托
+:param: currency：eth_btc 交易对
+:param: id：123456789  交易id
+:return: json 对象
+"""
+
+def exx_cancelOrder(currency, id):
+    try:
+        current_time = str(int(time.time() * 1000))
+        params = "&id=" + id + "&currency=" + currency
+        url = baseUrl + "cancel" + "?" + accesskey + params + "&nonce=" + current_time + \
+              "&signature=" + sha512_signature(params)
+        response = requests.get(url)
+        result = response.json()
+        print(result)
+    except Exception as e:
+        print("取消委托失败", e)
+    return result
+
+"""
+获取委托买单或卖单
+:param: id : 123456789 交易id
+:param: currency :eth_btc  交易对 
+:return: json 对象
+"""
+
+def getOrder(currency, id):
+    result = {}
+    try:
+        current_time = str(int(time.time() * 1000))
+        params = "&currency=" + currency + "&id=" + id
+        url = baseUrl + "getOrder" + "?" + accesskey + params + "&nonce=" + current_time + \
+              "&signature=" + sha512_signature(params)
+        print(url)
+        response = requests.get(url)
+        result = response.json()
+
+    except Exception as e:
+        print("获取委托买单或买单失败", e)
+    return result
+
+"""
+ 获取多个委托买单或卖单，每次请求返回10条记录
+:param:  pageIndex : 获取页数 
+:param: currency :eth_btc  交易对 
+:param:  type : buy/sell 交易类型
+:return: josn对象
+"""
+
+def getOpenOrders(currency, pageIndex, type):
+    try:
+        current_time = str(int(time.time() * 1000))
+        params = "&pageIndex=" + pageIndex + "&currency=" + currency + "&type=" + type
+        url = baseUrl + "getOpenOrders" + "?" + accesskey + params + "&nonce=" + current_time + \
+              "&signature=" + sha512_signature(params)
+        print(url)
+        response = requests.get(url)
+        result = response.json()
+    except Exception as e:
+        print("获取多个买单或卖单失败", e)
+    return result
+
+"""
+:param:null
+:url: https://trade.exx.com/api/getBalance?accesskey=your_access_key&nonce=当前时间毫秒数&signature=请求加密签名串
+:return
+"""
+
+def getBalance():
+    params = ""
+    current_time = str(int(time.time() * 1000))
+    url = baseUrl + "getBalance" + "?" + accesskey + "&nonce=" + current_time + \
+          "&signature=" + sha512_signature(params)
+    response = requests.get(url)
+    result = response.json()
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exchangeConnection/exx/util.py
@@ -0,0 +1,35 @@
+# _*_ codingLutf-8 _*_
+
+import hashlib
+import time
+import hmac
+import accountConfig
+
+# 在此输入您的Key
+ACCESS_KEY = accountConfig.EXX["ACCESS_KEY"]
+SECRET_KEY = accountConfig.EXX["SECRET_KEY"]
+SERVICE_API = accountConfig.EXX["SERVICE_API"]
+
+
+"""
+exx交易签名
+:param: amount 交易数量
+:param: currency:eth_usdt 交易的对
+:param: price:1024 价格
+:param: type:buy/sell 交易类型
+:return: int 交易id
+:return: bytes 加密签名 
+"""
+def sha512_signature(params):
+    current_time = str(int(time.time() * 1000))
+    # 拼接加密参数
+    # param = "&amount" + amount + "&currency" + currency + "&price" + price + "&type" + type
+    # 拼接assckey、时间、参数
+    str_params = ACCESS_KEY + params + "&nonce=" + current_time
+    list_parmas = str_params.split("&")
+    sort_parmas = sorted(list_parmas)
+    singature_parmas = "&".join(sort_parmas)
+    singature_parmas = bytes(singature_parmas, encoding="utf-8")
+    signature = hmac.new(SECRET_KEY, singature_parmas, hashlib.sha512).hexdigest()
+
+    return signature
