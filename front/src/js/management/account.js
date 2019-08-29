bae077098b32f571a1a279d607ab1d49d9572c44
daiqile <daiqile@xwzsz.com>
2019-08-29 19:08:18 +0800

    account.js文件

---
+++ front/src/js/management/account.js
@@ -0,0 +1,21 @@
+function Account() {
+    var accountWrapper = $('.add-account-wrapper');
+    var curryWrapper = $('.account-curry-wrapper');
+    var totalProperty = $('.property-total-wrapper');
+    var detailsProperty = $('.property-details-wrapper');
+
+};
+
+
+Account.prototype.run = function () {
+    var self = this;
+}
+
+Account.prototype.listenShowAddAccount = function(){
+    var self = this;
+
+}
+$(function () {
+    var account = new Account();
+    account.run();
+})
