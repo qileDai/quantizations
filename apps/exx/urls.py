3add9cdd9cf82ee1c3d80f436222fc146c2928f8
daiqile <daiqile@xwzsz.com>
2019-08-30 16:32:17 +0800

    网格交易机器人

--- apps/exx/urls.py
+++ apps/exx/urls.py
@@ -3,6 +3,8 @@ from . import views
 app_name = 'exx'
 urlpatterns = [
     path('trading_account/',views.trading_account,name='trading_account'),
+    path('girding/',views.girding,name='girding'),
+    path('index/',views.index,name='index'),
 
 
 ]
--- apps/exx/views.py
+++ apps/exx/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def trading_account(request):
     return render(request,'management/tradingaccount.html')
+
+def girding(request):
+    return render(request,'management/gridding.html')
+
+def index(request):
+    return render(request,'management/index.html')
