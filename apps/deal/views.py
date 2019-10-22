df3a9a8482588b91e793537b56942ebe029caa20
lixiangyun <lixiangyun@xwzsz.com>
2019-10-22 22:44:31 +0800

    策略

--- apps/deal/views.py
+++ apps/deal/views.py
@@ -746,6 +746,8 @@ class RobotList(generics.CreateAPIView):
 
     def get(self, request):
         pageNum = int(request.GET.get('pageIndex', 1))
+        if pageNum is None:
+            return restful.params_error(message='参数为空')
         pagesize = request.GET.get('pageSize')
         # 拿到下拉框交易币种值
         curry = request.GET.get('deal-curry')
@@ -848,35 +850,35 @@ class RobotYield(generics.CreateAPIView):
         return restful.result(data=serialize.data)
 
 
-# 分页
-def get_pagination_data(paginator, page_obj, around_count=2):
-    current_page = page_obj.number
-    num_pages = paginator.num_pages
-
-    left_has_more = False
-    right_has_more = False
-
-    if current_page <= around_count + 2:
-        left_pages = range(1, current_page)
-    else:
-        left_has_more = True
-        left_pages = range(current_page - around_count, current_page)
-
-    if current_page >= num_pages - around_count - 1:
-        right_pages = range(current_page + 1, num_pages + 1)
-    else:
-        right_has_more = True
-        right_pages = range(current_page + 1, current_page + around_count + 1)
-
-
-
-    return {
-        # left_pages：代表的是当前这页的左边的页的页码
-        'left_pages': left_pages,
-        # right_pages：代表的是当前这页的右边的页的页码
-        'right_pages': right_pages,
-        'current_page': current_page,
-        'left_has_more': left_has_more,
-        'right_has_more': right_has_more,
-        'num_pages': num_pages
-    }
+# # 分页
+# def get_pagination_data(paginator, page_obj, around_count=2):
+#     current_page = page_obj.number
+#     num_pages = paginator.num_pages
+#
+#     left_has_more = False
+#     right_has_more = False
+#
+#     if current_page <= around_count + 2:
+#         left_pages = range(1, current_page)
+#     else:
+#         left_has_more = True
+#         left_pages = range(current_page - around_count, current_page)
+#
+#     if current_page >= num_pages - around_count - 1:
+#         right_pages = range(current_page + 1, num_pages + 1)
+#     else:
+#         right_has_more = True
+#         right_pages = range(current_page + 1, current_page + around_count + 1)
+#
+#
+#
+#     return {
+#         # left_pages：代表的是当前这页的左边的页的页码
+#         'left_pages': left_pages,
+#         # right_pages：代表的是当前这页的右边的页的页码
+#         'right_pages': right_pages,
+#         'current_page': current_page,
+#         'left_has_more': left_has_more,
+#         'right_has_more': right_has_more,
+#         'num_pages': num_pages
+#     }
